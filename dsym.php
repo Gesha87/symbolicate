c4f506f09418b8d55a71715a1e877fc1b12f3f5e
Andrey Kulik <kulik.andrei87@gmail.com>
2015-03-19 14:15:08 +0600

    1

--- dsym.php
+++ dsym.php
@@ -5,8 +5,9 @@ if (empty($hash) || empty($dsym) || empty($dsym['size'])) {
 	header("HTTP/1.0 400 Bad request");
 	exit;
 }
-$fileName = '/var/dsyms/'.$hash.'/'.$dsym['name'];
-if (!move_uploaded_file($dsym['tmp_name'], $fileName)) {
+$dirName = '/var/dsyms/'.$hash;
+$fileName = $dirName.'/'.$dsym['name'];
+if (!mkdir($dirName) || !move_uploaded_file($dsym['tmp_name'], $fileName)) {
 	header("HTTP/1.0 403 Access denied");
 	exit;
 }
